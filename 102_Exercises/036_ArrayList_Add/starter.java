15a626973b8d96eb0b6e2084145412a9381edc90
sevookk <danny.hacobian@gmail.com>
2024-02-20 08:31:01 -0800

    Exercise 36

---
+++ 102_Exercises/036_ArrayList_Add/starter.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class starter {
+	public static void main(String args[]){
+		// Create an ArrayList
+		// Call addValuesArrayList on it with a user defined number of values (use Scanner)
+		// Call printArrayList on it
+
+		// Create a second ArrayList
+		// Call addValuesBackwards with the user input number of values
+		// Call printArrayList on it
+	}
+
+	public static void addValuesArrayList(ArrayList<Integer> arr, int n){
+		// This method should add n number of random values between 0-10 (10 not inclusive) to the ArrayList
+	}
+
+	public static void printArrayList(ArrayList<Integer> arr){
+		// This method should print all values in an ArrayList given.
+	}
+
+	public static void addValuesBackwards(ArrayList<Integer> arr, int n){
+		// This method should add values to the ArrayList in ascending order but print in descending order.
+	}
+}
