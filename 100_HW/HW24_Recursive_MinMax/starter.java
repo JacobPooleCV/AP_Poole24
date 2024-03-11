fae64fb84e3c4e0089793c693b04033757aabcf7
JacobPooleCV <jpoole@gusd.net>
2024-03-11 09:21:30 -0700

    hw24

---
+++ 100_HW/HW24_Recursive_MinMax/starter.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class starter {
+	public static void main(String args[]) {
+		// Create an array of 20 numbers
+		// Print your array
+		// Print the minimum of your array
+		// Print the maximum of your array
+
+	}
+
+	public static int getMin(int[] arr, int i){
+		// Parameter - arr is your array that you're searching through
+		// Parameter - i is the value you're currently on
+		// Return - Return the minimum of the entire array
+		// Base Case - When i reaches your length-1, return the minimum of the last 2 numbers
+		// While you still have numbers left, find the smaller of the number you're on and the getMin of the rest of the array (not including the number you're on)
+	}
+
+	public static int getMax(int[] arr, int i){
+		// Parameter - arr is your array that you're searching through
+		// Parameter - i is the value you're currently on
+		// Return - Return the maximum of the entire array
+		// Base Case - When i reaches your length-1, return the maximum of the last 2 numbers
+		// While you still have numbers left, find the larger of the number you're on and the getMin of the rest of the array (not including the number you're on)
+	}
+
+	public static void genArray(int[] arr){
+		for(int i = 0; i < arr.length; i++){
+			arr[i] = (int)(Math.random()*100);
+		}
+	}
+
+	public static void printArray(int[] arr){
+		System.out.println("------------------------");
+		for(int i: arr){
+			System.out.print(i + ", ");
+		}
+		System.out.println();
+		System.out.println("------------------------");
+	}
+}
