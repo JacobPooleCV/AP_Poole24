900fbd34ee1eed5726d865ad0565b95fa2cec060
sevookk <danny.hacobian@gmail.com>
2024-03-13 08:36:10 -0700

    E38

---
+++ 102_Exercises/038_Recur_Digits/pkg/BaseClass.java
@@ -0,0 +1,17 @@
+/*
+ *	Author:  
+ *  Date: 
+*/
+
+package pkg;
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class BaseClass {
+	public BaseClass() {
+		
+		
+	}
+
+}
---
+++ 102_Exercises/038_Recur_Digits/starter.java
@@ -0,0 +1,17 @@
+/*
+ *	Author:  
+ *  Date: 
+*/
+
+import java.util.Scanner;
+import java.util.Random;
+
+class starter {
+	public static void main(String args[]) {
+		// Your code goes below here
+		int x = Integer.MAX_VALUE;
+
+
+		
+	}
+}
