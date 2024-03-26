ceae66d675fe441fb2b7d62dc2309d0e8f3370db
JacobPoole <jacob.poole589@gmail.com>
2024-03-25 22:35:44 -0700

    HW25

---
+++ 100_HW/HW25_Palindrome_Creator/starter.java
@@ -0,0 +1,21 @@
+/*
+	Author:
+	Date: 
+*/
+
+import java.util.*;
+
+class starter {
+	public static void main(String args[]) {
+		// Ask for a word via Scanner
+		// Call createPalindrome on that word
+	}
+
+	public static String createPalindrome(String word){
+		// Input: potato
+		// Output: otatoppotato
+		// Recursive Algorithm
+		// Base case - If one letter is left, just print the letter out twice
+		// While the String word has more than one letter, break off the last letter and add it to the string returned
+	}
+}
